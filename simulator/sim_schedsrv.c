198a68ff0eb4ecbe9f61880e04d540987955c902
Mark A. Grondona <mgrondona@llnl.gov>
2015-05-11 08:20:01 -0700

    simulator: remove unused function set_event
    Unused static function set_event() is causing build error with clang
    and -Werror. Comment it out for now.

--- simulator/sim_schedsrv.c
+++ simulator/sim_schedsrv.c
@@ -313,6 +313,7 @@ int update_job_state (flux_lwj_t *job, lwj_event_e e)
     return rc;
 }
 
+#if 0
 static inline void
 set_event (flux_event_t *e,
            event_class_e c, int ei, flux_lwj_t *j)
@@ -332,6 +333,7 @@ set_event (flux_event_t *e,
     }
     return;
 }
+#endif
 
 
 static int
