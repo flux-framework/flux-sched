ba72310cea69ddc6f38da7a3e31a97c4769d0db9
Mark Grondona <mark.grondona@gmail.com>
2016-02-20 21:17:17 +0000

    testsuite: pull flux-sharness.sh from flux-core
    Update t/sharness.d/flux-sharness.sh from upstream flux-core.

--- t/sharness.d/flux-sharness.sh
+++ t/sharness.d/flux-sharness.sh
@@ -4,22 +4,6 @@
 #
 
 #
-#  Unset variables important to Flux
-#
-unset FLUX_CONFIG
-unset FLUX_MODULE_PATH
-unset FLUX_CMBD_PATH
-
-#
-#  FLUX_PREFIX identifies the prefix for the flux we will be testing
-#
-FLUX_PREFIX=`pkg-config --variable=prefix flux-core`
-
-if test -z "${FLUX_PREFIX}" ; then
-    FLUX_PREFIX=/usr
-fi
-
-#
 #  Extra functions for Flux testsuite
 #
 run_timeout() {
@@ -27,20 +11,32 @@ run_timeout() {
 }
 
 #
+#  Echo on stdout a reasonable size for a large test session,
+#   controllable test-wide via env vars FLUX_TEST_SIZE_MIN and
+#   FLUX_TEST_SIZE_MAX.
+#
+test_size_large() {
+    min=${FLUX_TEST_SIZE_MIN:-4}
+    max=${FLUX_TEST_SIZE_MAX:-17}
+    size=$(($(nproc)+1))
+    test ${size} -lt ${min} && size=$min
+    test ${size} -gt ${max} && size=$max
+    echo ${size}
+}
+
+#
 #  Reinvoke a test file under a flux comms instance
 #
-#  Usage: test_under_flux <size> <flux-sched root dir>
+#  Usage: test_under_flux <size>
 #
 test_under_flux() {
-    size=$1
-    tdir=$2
+    size=${1:-1}
     log_file="$TEST_NAME.broker.log"
     if test -n "$TEST_UNDER_FLUX_ACTIVE" ; then
         cleanup rm "${SHARNESS_TEST_DIRECTORY:-..}/$log_file"
-        unset TEST_UNDER_FLUX_ACTIVE
         return
     fi
-    quiet="-o -q,-Slog-filename=${log_file}"
+    quiet="-o -q,-Slog-filename=${log_file},-Slog-forward-level=7"
     if test "$verbose" = "t" -o -n "$FLUX_TESTS_DEBUG" ; then
         flags="${flags} --verbose"
         quiet=""
@@ -57,9 +53,6 @@ test_under_flux() {
 
     TEST_UNDER_FLUX_ACTIVE=t \
     TERM=${ORIGINAL_TERM} \
-    LUA_PATH="${tdir}/rdl/?.lua;${FLUX_PREFIX}/share/lua/5.1/?.lua;${LUA_PATH};;" \
-    LUA_CPATH="${tdir}/rdl/.libs/?.so;${FLUX_PREFIX}/lib64/lua/5.1/?.so;${LUA_CPATH};;" \
-    FLUX_MODULE_PATH="${tdir}/sched" \
       exec flux start --size=${size} ${quiet} "sh $0 ${flags}"
 }
 
@@ -83,39 +76,13 @@ test_on_rank() {
     flux exec --rank=${ranks} "$@"
 }
 
-
-#
-#  Export some extra variables to test scripts specific to Flux
-#   testsuite
-#
-#  Add path to flux(1) command to PATH
-#
-if test -n "$FLUX_TEST_INSTALLED_PATH"; then
-    PATH=$FLUX_TEST_INSTALLED_PATH:$PATH
-    fluxbin=$FLUX_TEST_INSTALLED_PATH/flux
-else # normal case, use $FLUX_PREFIX
-    PATH=$FLUX_PREFIX/bin:$PATH
-    fluxbin=$FLUX_PREFIX/bin/flux
-fi
-export PATH
-
-if ! test -x ${fluxbin}; then
-    echo >&2 "Failed to find a flux binary in ${fluxbin}."
-    echo >&2 "Do you need to run make?"
-    return 1
-fi
-
 #  Export a shorter name for this test
 TEST_NAME=$SHARNESS_TEST_NAME
 export TEST_NAME
 
 #  Test requirements for testsuite
-if ! lua -e 'require "flux.posix"'; then
+if ! lua -e 'require "posix"'; then
     error "failed to find lua posix module in path"
 fi
-if ! lua -e 'require "flux.hostlist"'; then
-    error "failed to find lua hostlist module in path"
-fi
-
 
 # vi: ts=4 sw=4 expandtab
