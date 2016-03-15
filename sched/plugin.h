57cedf6e4e18f3894694775726eb423ebb5240df
Jim Garlick <garlick.jim@gmail.com>
2016-03-15 13:36:25 -0700

    sched: add dynamic plugin loading
    Factor out plugin loading functions to plugin.[ch], creating
    a semi-abstract plugin loader.
    
    Retain the default fcfs plugin loading behavior, with sched module
    command line override.
    
    Add insmod, rmmod, lsmod handlers that operate within the plugin
    loader.  Now this works:
    
      $ flux module load sched
      $ flux module list sched
      Module               Size    Digest  Idle  S  Nodeset
      sched.fcfs             43228 CD51E2B    0  R  0
      $ flux module remove sched.fcfs
      $ flux module list sched
      Module               Size    Digest  Idle  S  Nodeset
      $ flux module load sched.backfill
      $ flux module list sched
      Module               Size    Digest  Idle  S  Nodeset
      sched.backfill         47843 D775294    0  R  0
    
    Assertions were placed at locations in sched where the plugin is
    required but (now) may be unloaded.  Hopefully it's possible to
    "pause" scheduler operations until a new plugin can be loaded?

---
+++ sched/plugin.h
@@ -0,0 +1,59 @@
+#ifndef _FLUX_SCHED_PLUGIN_H
+#define _FLUX_SCHED_PLUGIN_H
+
+struct sched_plugin {
+    void         *dso;                /* Scheduler plug-in DSO handle */
+    char         *name;               /* Name of plugin */
+    char         *path;               /* Path to plugin dso */
+
+    int                  (*sched_loop_setup)(flux_t h);
+
+    resrc_tree_list_t *  (*find_resources)(flux_t h,
+                                           resrc_t *resrc,
+                                           resrc_reqst_t *resrc_reqst);
+
+    resrc_tree_list_t *  (*select_resources)(flux_t h,
+                                             resrc_tree_list_t *resrc_trees,
+                                             resrc_reqst_t *resrc_reqst);
+
+    int                  (*allocate_resources)(flux_t h,
+                                               resrc_tree_list_t *rtl,
+                                               int64_t job_id,
+                                               int64_t starttime,
+                                               int64_t endtime);
+
+    int                  (*reserve_resources)(flux_t h,
+                                              resrc_tree_list_t *rtl,
+                                              int64_t job_id,
+                                              int64_t starttime,
+                                              int64_t walltime,
+                                              resrc_t *resrc,
+                                              resrc_reqst_t *resrc_reqst);
+};
+
+/* Create/destroy the plugin loader apparatus.
+ */
+struct sched_plugin_loader;
+struct sched_plugin_loader *sched_plugin_loader_create (flux_t h);
+void sched_plugin_loader_destroy (struct sched_plugin_loader *sploader);
+
+/* Load plugin by name.
+ * 'name' may be either a file pathname (contains / chars) or a plugin name
+ * like "sched.fifo".
+ */
+int sched_plugin_load (struct sched_plugin_loader *sploader, const char *name);
+
+/* Unload the currently loaded plugin.
+ */
+void sched_plugin_unload (struct sched_plugin_loader *sploader);
+
+/* Retrieve the currently loaded plugin.
+ * Return NULL if plugin is not loaded.
+ */
+struct sched_plugin *sched_plugin_get (struct sched_plugin_loader *sploader);
+
+#endif /* !_FLUX_SCHED_PLUGIN_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
