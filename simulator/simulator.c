cdd201e3f448c665aaa7ff9a470e136c1c6550ee
Mark A. Grondona <mgrondona@llnl.gov>
2016-10-15 14:27:52 -0700

    simulator: add job_kvsdir() function
    Add a function to return a kvsdir to a given jobid id, first querying
    job.kvspath service to get path to kvs for id, and if kvspath service
    not active, then fall back to lwj.%d.
    
    Export via simulator.h.

--- simulator/simulator.c
+++ simulator/simulator.c
@@ -292,3 +292,28 @@ zhash_t *zhash_fromargv (int argc, char **argv)
     }
     return args;
 }
+
+// Get a kvsdir handle to jobid [jobid] using job.kvspath service.
+//  If service doesn't answer, fall back to `lwj.%d`
+kvsdir_t *job_kvsdir (flux_t *h, int jobid)
+{
+    flux_rpc_t *rpc;
+    const char *kvs_path;
+    kvsdir_t *d = NULL;
+
+    rpc = flux_rpcf (h, "job.kvspath", FLUX_NODEID_ANY, 0,
+                    "{s:[i]}", "ids", jobid);
+    if (!rpc) {
+        flux_log_error (h, "flux_rpcf");
+        return (NULL);
+    }
+    if (flux_rpc_getf (rpc, "{s:[s]}", "paths", &kvs_path) < 0) {
+        // Fall back to lwj.%d:
+        if (kvs_get_dir (h, &d, "lwj.%d", jobid))
+            flux_log_error (h, "kvs_get_dir (lwj.%d)", jobid);
+    }
+    else if (kvs_get_dir (h, &d, "%s", kvs_path) < 0)
+        flux_log_error (h, "kvs_get_dir");
+    flux_rpc_destroy (rpc);
+    return (d);
+}
--- simulator/simulator.h
+++ simulator/simulator.h
@@ -55,6 +55,7 @@ void free_simstate (sim_state_t *sim_state);
 json_t *sim_state_to_json (sim_state_t *state);
 sim_state_t *json_to_sim_state (json_t *o);
 
+kvsdir_t *job_kvsdir (flux_t *h, int jobid);
 int put_job_in_kvs (job_t *job);
 job_t *pull_job_from_kvs (int id, kvsdir_t *kvs_dir);
 void free_job (job_t *job);
