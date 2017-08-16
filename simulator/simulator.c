c87b84e17dc275d960663d3eac85dc650f4f1c3b
Samuel Pollard <pollard4@llnl.gov>
2017-08-16 12:10:14 -0700

    Removed spurious error in simulator

--- simulator/simulator.c
+++ simulator/simulator.c
@@ -210,9 +210,10 @@ int kvsdir_get_double (kvsdir_t *dir, const char *name, double *valp)
         goto done;
     rc = 0;
 done:
-    if (rc < 0)
-        flux_log_error (h, "pull_job_from_kvs: failed to get %s in %s",
-                        name, kvsdir_key (dir));
+    // FIXME: This is happening more than it should
+    // if (rc < 0)
+    //     flux_log_error (h, "pull_job_from_kvs: failed to get %s in %s",
+    //                     name, kvsdir_key (dir));
     free (key);
     flux_future_destroy (f);
     return rc;
