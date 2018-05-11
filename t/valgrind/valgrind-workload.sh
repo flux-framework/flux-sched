293ffdbfeb1c3b8155e0bc6d387930d4337daa72
Mark A. Grondona <mark.grondona@gmail.com>
2018-05-11 10:09:46 -0700

    testsuite: ensure last job has completed in valgrind test
    Synchronize to ensure last flux-wreckrun job has reached the
    completed state in the t5000-valgrind.t test to avoid racing
    with the sched module releasing memory for complete jobs.

--- t/valgrind/valgrind-workload.sh
+++ t/valgrind/valgrind-workload.sh
@@ -10,4 +10,8 @@ for i in `seq 1 $NJOBS`; do
     flux wreckrun --ntasks $size /bin/true
 done
 
+# Wait up to 5s for last job to be fully complete before removing sched module:
+KEY=$(echo $(flux wreck last-jobid -p).state)
+${SHARNESS_TEST_SRCDIR}/scripts/kvs-watch-until.lua -t 5 $KEY 'v == "complete"'
+
 flux module remove sched
