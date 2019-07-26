33fc23cf05c481387561b7ab5771417c00930bac
Dong H. Ahn <ahn1@llnl.gov>
2019-07-25 17:02:02 -0700

    queue: Add HYBRID/CONSERVATIVE backfillig support
    CONSERVATIVE backfilling: A less aggressive alternative
    to EASY with similar performance. It determines an allocation
    for each job when it enters the system. Then a job can be a candidate
    to backfilling􏰁if and only if it can begin its execution immediately
    without delaying any of the other pre-allocated jobs.
    
    HYBRID backfilling: An algorithm lies in between EASY and
    CONSERVATIVE backfilling strategies. It allows for limiting
    the number of waiting jobs that must have a starting time
    guarantee to be K where K is configurable. K must be greater than
    1 and less than the system max.
    
    Implement HYBRID in queue_policy_hybrid.hpp and
    queue_policy_bybrid_impl.hpp and CONSERVATIVE
    in queue_policy_conservative.hpp and
    queue_policy_conservative_impl.hpp.
    
    Derived from the backfill base class while specifializing
    it with reservation-depth=system-max or reservation-depth=K
    where K is configurable.

---
+++ qmanager/config/queue_system_defaults.hpp
@@ -0,0 +1,42 @@
+/*****************************************************************************\
+ *  Copyright (c) 2014 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
+
+#ifndef QUEUE_SYSTEM_DEFAULT_HPP
+#define QUEUE_SYSTEM_DEFAULT_HPP
+
+// FIXME: These need to be coverted into a scheduler configuration file
+namespace Flux {
+namespace queue_manager {
+namespace detail {
+    const unsigned int MAX_RESERVATION_DEPTH = 4096;
+    const unsigned int HYBRID_RESERVATION_DEPTH = 64;
+} // namespace detail
+} // namespace resource_model
+} // namespace Flux
+
+#endif // QUEUE_SYSTEM_DEFAULT_HPP
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
