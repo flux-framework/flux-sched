c18484a3310b646f4ac8afdec6f48c8ca6858992
Jim Garlick <garlick.jim@gmail.com>
2020-03-25 10:49:01 -0700

    testsuite: don't set FLUX_CONF_DIR
    Problem: Since flux-core no longer has a default config file
    file location, the setting of FLUX_CONF_DIR in sched-sharness.sh
    is no longer needed.
    
    Drop the line that sets this variable.
    Drop the line that unsets it in t1005-qmanager-conf.t.

--- t/sharness.d/sched-sharness.sh
+++ t/sharness.d/sched-sharness.sh
@@ -17,7 +17,6 @@ else
   FLUX_MODULE_PATH_PREPEND="${SHARNESS_BUILD_DIRECTORY}/resource/modules/.libs"
   FLUX_MODULE_PATH_PREPEND="${SHARNESS_BUILD_DIRECTORY}/qmanager/modules/.libs":${FLUX_MODULE_PATH_PREPEND}
   FLUX_EXEC_PATH_PREPEND="${SHARNESS_TEST_SRCDIR}/scripts":"${SHARNESS_TEST_SRCDIR}/../src/cmd"
-  export FLUX_CONF_DIR=${SHARNESS_TEST_SRCDIR}/../etc
 fi
 
 ## Set up environment using flux(1) in PATH
