53998ab7818fe139ad1a443134ed78652580853b
Dong H. Ahn <ahn1@llnl.gov>
2020-04-22 10:19:38 -0700

    qmanager: Add qmanager option-set class
    Refactor qmanager option set management into its own class
    (qmanager_opts_t).
    
    Use and comply with the newly added generic module option
    set management and parsing utilities:
        - Implement operator+=() required by the optmgr_composer_t
          class so that an option-set object can be composed with
          another option-set object with a higher precedence.
        - Implement parse() (required by optmgr_kv_t) which parses
          each and every option key-value pair passed
          from optmgr_kv_t (representing an entire option set
          from a single source (e.g., configuration file).

---
+++ qmanager/modules/qmanager_opts.cpp
@@ -0,0 +1,184 @@
+/*****************************************************************************\
+ *  Copyright (c) 2020 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
+
+#include "qmanager_opts.hpp"
+
+using namespace Flux;
+using namespace Flux::opts_manager;
+
+
+/******************************************************************************
+ *                                                                            *
+ *              Private API for Queue Manager Option Class                    *
+ *                                                                            *
+ ******************************************************************************/
+
+qmanager_opts_t &qmanager_opts_t::canonicalize ()
+{
+    // Placeholder: If there are options whose default values are
+    // determined by another option, this method must
+    // cannoicalize the state.
+    return *this;
+}
+
+bool qmanager_opts_t::known_queue_policy (const std::string &policy)
+{
+    bool rc = false;
+    if (policy == "fcfs" || policy == "easy"
+        || policy == "hybrid" || policy == "conservative")
+        rc = true;
+    return rc;
+}
+
+
+/******************************************************************************
+ *                                                                            *
+ *              Public API for Queue Manager Option Class                     *
+ *                                                                            *
+ ******************************************************************************/
+
+qmanager_opts_t::qmanager_opts_t ()
+{
+    // Note: std::pair<>() is guaranteed to throw only an exception
+    // from its arguments which in this case std::bad_alloc -- i.e.,
+    // from std::string (const char *).
+    // map<>::insert() can also throw std::bad_alloc.
+
+    bool inserted = true;
+
+    auto ret = m_tab.insert (std::pair<std::string, int> (
+                   "queue-policy",
+                   static_cast<int> (qmanager_opts_key_t::QUEUE_POLICY)));
+    inserted &= ret.second;
+    ret = m_tab.insert (std::pair<std::string, int> (
+              "queue-params",
+              static_cast<int> (qmanager_opts_key_t::QUEUE_PARAMS)));
+    inserted &= ret.second;
+    ret= m_tab.insert (std::pair<std::string, int> (
+              "policy-params",
+              static_cast<int> (qmanager_opts_key_t::POLICY_PARAMS)));
+    inserted &= ret.second;
+
+    if (!inserted)
+        throw std::bad_alloc ();
+}
+
+void qmanager_opts_t::set_queue_policy (const std::string &o)
+{
+    m_queue_prop.queue_policy = o;
+}
+
+void qmanager_opts_t::set_queue_params (const std::string &o)
+{
+    m_queue_prop.queue_params = o;
+}
+
+void qmanager_opts_t::set_policy_params (const std::string &o)
+{
+    m_queue_prop.policy_params = o;
+}
+
+const std::string &qmanager_opts_t::get_queue_policy () const
+{
+    return m_queue_prop.queue_policy;
+}
+
+const std::string &qmanager_opts_t::get_queue_params () const
+{
+    return m_queue_prop.queue_params;
+}
+
+const std::string &qmanager_opts_t::get_policy_params () const
+{
+    return m_queue_prop.policy_params;
+}
+
+bool qmanager_opts_t::is_queue_policy_set () const
+{
+    return m_queue_prop.queue_policy != QMANAGER_OPTS_UNSET_STR;
+}
+
+bool qmanager_opts_t::is_queue_params_set () const
+{
+    return m_queue_prop.queue_params != QMANAGER_OPTS_UNSET_STR;
+}
+
+bool qmanager_opts_t::is_policy_params_set () const
+{
+    return m_queue_prop.policy_params != QMANAGER_OPTS_UNSET_STR;
+}
+
+qmanager_opts_t &qmanager_opts_t::operator+= (const qmanager_opts_t &src)
+{
+    if (src.m_queue_prop.queue_policy != QMANAGER_OPTS_UNSET_STR)
+        m_queue_prop.queue_policy = src.m_queue_prop.queue_policy;
+    if (src.m_queue_prop.queue_params != QMANAGER_OPTS_UNSET_STR)
+        m_queue_prop.queue_params = src.m_queue_prop.queue_params;
+    if (src.m_queue_prop.policy_params != QMANAGER_OPTS_UNSET_STR)
+        m_queue_prop.policy_params = src.m_queue_prop.policy_params;
+    return canonicalize ();
+}
+
+int qmanager_opts_t::parse (const std::string &k, const std::string &v,
+                            std::string &info)
+{
+    int rc = 0;
+    std::string dflt;
+    int key = static_cast<int> (qmanager_opts_key_t::UNKNOWN);
+
+    if (m_tab.find (k) != m_tab.end ())
+        key = m_tab[k];
+
+    switch (key) {
+    case static_cast<int> (qmanager_opts_key_t::QUEUE_POLICY):
+        dflt = m_queue_prop.queue_policy;
+        m_queue_prop.queue_policy = v;
+        if (!known_queue_policy (m_queue_prop.queue_policy)) {
+            info += "Unknown queuing policy (" + v + ")! ";
+            info += "Use default.";
+            m_queue_prop.queue_policy = dflt;
+        }
+        break;
+
+    case static_cast<int> (qmanager_opts_key_t::QUEUE_PARAMS):
+        m_queue_prop.queue_params = v;
+        break;
+
+    case static_cast<int> (qmanager_opts_key_t::POLICY_PARAMS):
+        m_queue_prop.policy_params = v;
+        break;
+
+    default:
+        info += "Unknown option (" + k + ").";
+        errno = EINVAL;
+        rc = -1;
+        break;
+    }
+
+    return rc;
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ qmanager/modules/qmanager_opts.hpp
@@ -0,0 +1,112 @@
+/*****************************************************************************\
+ *  Copyright (c) 2020 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
+
+#ifndef QMANAGER_OPTS_HPP
+#define QMANAGER_OPTS_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+#include "src/common/liboptmgr/optmgr.hpp"
+
+namespace Flux {
+namespace opts_manager {
+
+const std::string QMANAGER_OPTS_UNSET_STR = "0xdeadbeef";
+
+struct queue_prop_t {
+    std::string queue_policy = QMANAGER_OPTS_UNSET_STR;
+    std::string queue_params = QMANAGER_OPTS_UNSET_STR;
+    std::string policy_params = QMANAGER_OPTS_UNSET_STR;
+};
+
+/*! qmanager option set class
+ *
+ */
+class qmanager_opts_t : public optmgr_parse_t {
+public:
+    enum class qmanager_opts_key_t : int {
+        QUEUE_POLICY              = 10, // queue-policy
+        QUEUE_PARAMS              = 20, // queue-params
+        POLICY_PARAMS             = 30, // policy-params
+        UNKNOWN                   = 5000
+    };
+
+    // These constructors can throw std::bad_alloc exception
+    qmanager_opts_t ();
+    qmanager_opts_t (const qmanager_opts_t &o) = default;
+    qmanager_opts_t &operator= (const qmanager_opts_t &o) = default;
+    // No exception is thrown from move constructor and move assignment
+    qmanager_opts_t (qmanager_opts_t &&o) = default;
+    qmanager_opts_t &operator= (qmanager_opts_t &&o) = default;
+
+    void set_queue_policy (const std::string &o);
+    void set_queue_params (const std::string &o);
+    void set_policy_params (const std::string &o);
+
+    const std::string &get_queue_policy () const;
+    const std::string &get_queue_params () const;
+    const std::string &get_policy_params () const;
+
+    bool is_queue_policy_set () const;
+    bool is_queue_params_set () const;
+    bool is_policy_params_set () const;
+
+    /*! Add an option with a higher precendence than "this" object.
+     *  Modify this object according to the following "add" semantics
+     *  and return the modified object:
+     *  For each option key in o which has a non-default value,
+     *  override the same key in this object.
+     *
+     *  \param o         an option set object with a higher precendence.
+     *  \return          the composed qmanager_opts_t object.
+     */
+    qmanager_opts_t &operator+= (const qmanager_opts_t &o);
+
+    /*! Parse the value string (v) according to the key string (k).
+     *  The parsed results are stored in "this" object.
+     *
+     *  \param k         key string
+     *  \param v         value string
+     *  \param info      parsing info and warning string to return.
+     *  \return          0 on success; -1 on error.
+     */
+    int parse (const std::string &k, const std::string &v, std::string &info);
+
+private:
+    qmanager_opts_t &canonicalize ();
+    bool known_queue_policy (const std::string &policy);
+
+    queue_prop_t m_queue_prop;
+    std::map<std::string, int> m_tab;
+};
+
+} // namespace Flux
+} // namespace opts_manager
+
+#endif // QMANAGER_OPTS_HPP
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
