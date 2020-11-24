33c499b4c1e0d0506563194dc3902a526da8baac
Dong H. Ahn <ahn1@llnl.gov>
2020-11-23 21:55:24 -0800

    reader: add resource namespace remapper support
    Problem: The rank and resource Id namespace of a
    nested flux instance is different from that of its
    parent. Thus, the resource reader of a nested
    instance may need to remap the rank and/or certain
    resource Ids as it populates its resource graph
    store from the resources emitted from its parent.
    
    Add a header and C++ source file that introduces
    the resource_namespace_remapper_t class. It is
    designed to add and query resource Id
    or rank remapping information efficiently
    both in terms of performance and memory overheads.
    
    Using the add() method, users should first pass
    the resource type (e.g., "core", "gpu") or "rank" as
    the "name_type" argument along with the Id remapping
    info (ref_id is remapped to remapped_id) for each
    unique range of flux ranks (e.g., 0-3).
    
    resource_namespace_remapper_t keeps each
    remapping data using distinct_range_t as key to a
    std::map member object. IOW, we treat each distinct
    rank range which has the same remapping info as
    an equivalent set and keep only one copy
    of remapping info.
    
    Furthermore, the distinct_range_t class implements
    "operator<()" so that this mapping info can be kept
    in std::map in ascending rank-range order,
    which then allows data querying to be performed
    in O(logN) time.
    
    However, the method for querying is the query() method,
    whose semantics is per individual rank based.
    To support retrieval of remapping information
    for a individual rank, we implement "operator<()"
    in such a way that we can find the corresponding
    record when the rank intersects a rank-range key.

---
+++ resource/readers/resource_namespace_remapper.hpp
@@ -0,0 +1,80 @@
+/*****************************************************************************\
+ *  Copyright (c) 2020 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+ \*****************************************************************************/
+
+#ifndef RESOURCE_NAMESPACE_REMAPPER_HPP
+#define RESOURCE_NAMESPACE_REMAPPER_HPP
+
+#include <map>
+#include <string>
+#include <cstdint>
+
+namespace Flux {
+namespace resource_model {
+
+class resource_namespace_remapper_t {
+public:
+    int add (const uint64_t exec_target_high,
+             const uint64_t exec_target_low,
+             const std::string &name_type,
+             uint64_t ref_id, uint64_t remapped_id);
+    int add (const std::string &exec_target_range,
+             const std::string &name_type,
+             uint64_t ref_id, uint64_t remapped_id);
+    int query (const uint64_t exec_target,
+               const std::string &name_type,
+               uint64_t ref_id, uint64_t &remapped_id_out) const;
+    bool is_remapped () const;
+
+private:
+    class distinct_range_t {
+    public:
+        explicit distinct_range_t (uint64_t point);
+        distinct_range_t (uint64_t lo, uint64_t hi);
+
+        uint64_t get_low () const;
+        uint64_t get_high () const;
+        bool is_point () const;
+        bool operator< (const distinct_range_t &o) const;
+        bool operator== (const distinct_range_t &o) const;
+        bool operator!= (const distinct_range_t &o) const;
+
+    private:
+        uint64_t m_low;
+        uint64_t m_high;
+    };
+
+    std::map<const distinct_range_t,
+             std::map<const std::string,
+                      std::map<uint64_t, uint64_t>>> m_remap;
+};
+
+} // namesapce resource_model
+} // namespace Flux
+
+
+#endif // RESOURCE_NAMESPACE_REMAPPER_HPP
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
