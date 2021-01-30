f420c77cfd7f1a35769b998b7550fdc6d863fd86
Mark A. Grondona <mark.grondona@gmail.com>
2021-01-29 18:43:40 -0800

    testsuite: add flux-builtin-conf.py
    Add a script to query for flux "bulitin" configuration parameters,
    e.g. "shell_path", "man_path", "exec_path", etc. The valid values
    to query can be found in flux-core:src/common/libflux/conf.c

---
+++ t/scripts/flux-builtin-conf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+##############################################################
+# Copyright 2021 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+#
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+##############################################################
+import sys
+from flux.core.inner import raw
+from flux.constants import FLUX_CONF_AUTO;
+print(raw.flux_conf_builtin_get(sys.argv[1], FLUX_CONF_AUTO).decode("utf-8"))
