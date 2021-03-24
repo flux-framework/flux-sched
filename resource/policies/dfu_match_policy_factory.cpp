476920bfb6a22784d51345f683ec93675bd8b035
Dong H. Ahn <ahn1@llnl.gov>
2021-03-23 23:44:50 -0700

    match: make first match known to match policy layer
    Just use low-id-first policy at the match policy
    level. The more imporant logic of having to stop
    graph vertex as soon as all job requests are
    first matched will be implemented in other layers.

--- resource/policies/dfu_match_policy_factory.cpp
+++ resource/policies/dfu_match_policy_factory.cpp
@@ -31,7 +31,8 @@ namespace resource_model {
 bool known_match_policy (const std::string &policy)
 {
     bool rc = true;
-    if (policy != HIGH_ID_FIRST && policy != LOW_ID_FIRST
+    if (policy != FIRST_MATCH
+        && policy != HIGH_ID_FIRST && policy != LOW_ID_FIRST
         && policy != LOCALITY_AWARE && policy != VAR_AWARE)
         rc = false;
 
@@ -43,7 +44,9 @@ std::shared_ptr<dfu_match_cb_t> create_match_cb (const std::string &policy)
     std::shared_ptr<dfu_match_cb_t> matcher = nullptr;
 
     try {
-    if (policy == HIGH_ID_FIRST)
+    if (policy == FIRST_MATCH)
+        matcher = std::make_shared<low_first_t> ();
+    else if (policy == HIGH_ID_FIRST)
         matcher = std::make_shared<high_first_t> ();
     else if (policy == LOW_ID_FIRST)
         matcher = std::make_shared<low_first_t> ();
