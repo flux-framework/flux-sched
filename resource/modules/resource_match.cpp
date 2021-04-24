aa814342c8a4171638f1b7fd3c3e4b77a43e1737
Dong H. Ahn <ahn1@llnl.gov>
2021-04-23 20:07:42 -0700

    resource: add match_multi support
    Introduce the match_multi stream RPC callback
    that can take in multiple job requests, runs
    a match for each job request and reply each
    matching result to the sender of
    this stream RPC.
    
    The stream RPC is finished either when
    the job iteration is over, in which case
    ENODATA is returned to the sender, or
    when a match error including ENODEV
    or ENOBUSY is encountered during
    the match run.
    
    The multi rpc can be used to hide
    the RPC overhead between fluxion
    qmanager and resource.

--- resource/modules/resource_match.cpp
+++ resource/modules/resource_match.cpp
@@ -233,6 +233,9 @@ resource_ctx_t::~resource_ctx_t ()
 static void match_request_cb (flux_t *h, flux_msg_handler_t *w,
                               const flux_msg_t *msg, void *arg);
 
+static void match_multi_request_cb (flux_t *h, flux_msg_handler_t *w,
+                                    const flux_msg_t *msg, void *arg);
+
 static void update_request_cb (flux_t *h, flux_msg_handler_t *w,
                                const flux_msg_t *msg, void *arg);
 
@@ -272,6 +275,8 @@ static void ns_info_request_cb (flux_t *h, flux_msg_handler_t *w,
 static const struct flux_msg_handler_spec htab[] = {
     { FLUX_MSGTYPE_REQUEST,
       "sched-fluxion-resource.match", match_request_cb, 0 },
+    { FLUX_MSGTYPE_REQUEST,
+       "sched-fluxion-resource.match_multi", match_multi_request_cb, 0 },
     { FLUX_MSGTYPE_REQUEST,
       "sched-fluxion-resource.update", update_request_cb, 0},
     { FLUX_MSGTYPE_REQUEST,
@@ -1809,6 +1814,80 @@ static void match_request_cb (flux_t *h, flux_msg_handler_t *w,
     return;
 
 error:
+    if (flux_respond_error (h, msg, errno, NULL) < 0)
+        flux_log_error (h, "%s: flux_respond_error", __FUNCTION__);
+}
+
+static void match_multi_request_cb (flux_t *h, flux_msg_handler_t *w,
+                                    const flux_msg_t *msg, void *arg)
+{
+    size_t index;
+    json_t *value;
+    json_error_t err;
+    int saved_errno;
+    json_t *jobs = nullptr;
+    const char *cmd = nullptr;
+    const char *jobs_str = nullptr;
+    std::shared_ptr<resource_ctx_t> ctx = getctx ((flux_t *)arg);
+
+    if (!flux_msg_is_streaming (msg)) {
+        errno = EPROTO;
+        goto error;
+    }
+    if (flux_request_unpack (msg, NULL, "{s:s s:s}",
+                                          "cmd", &cmd,
+                                          "jobs", &jobs_str) < 0)
+        goto error;
+    if ( !(jobs = json_loads (jobs_str, 0, &err))) {
+        errno = ENOMEM;
+        goto error;
+    }
+
+    json_array_foreach(jobs, index, value) {
+        uint64_t jobid;
+        const char *js_str;
+        int64_t at = 0;
+        int64_t now = 0;
+        double ov = 0.0f;
+        std::string status = "";
+        std::stringstream R;
+
+        if (json_unpack (value, "{s:I s:s}",
+                                  "jobid", &jobid,
+                                  "jobspec", &js_str) < 0)
+            goto error;
+        if (is_existent_jobid (ctx, jobid)) {
+            errno = EINVAL;
+            flux_log_error (h, "%s: existent job (%jd).",
+                            __FUNCTION__, static_cast<intmax_t> (jobid));
+            goto error;
+        }
+        if (run_match (ctx, jobid, cmd, js_str, &now, &at, &ov, R) < 0) {
+            if (errno != EBUSY && errno != ENODEV)
+                flux_log_error (ctx->h,
+                        "%s: match failed due to match error (id=%jd)",
+                        __FUNCTION__, static_cast<intmax_t> (jobid));
+            goto error;
+        }
+
+        status = get_status_string (now, at);
+        if (flux_respond_pack (h, msg, "{s:I s:s s:f s:s s:I}",
+                                         "jobid", jobid,
+                                         "status", status.c_str (),
+                                         "overhead", ov,
+                                         "R", R.str ().c_str (),
+                                         "at", at) < 0) {
+            flux_log_error (h, "%s", __FUNCTION__);
+            goto error;
+        }
+    }
+    errno = ENODATA;
+error:
+    if (jobs) {
+        saved_errno = errno;
+        json_decref (jobs);
+        errno = saved_errno;
+    }
     if (flux_respond_error (h, msg, errno, NULL) < 0)
         flux_log_error (h, "%s: flux_respond_error", __FUNCTION__);
 }
