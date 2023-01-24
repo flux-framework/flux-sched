25702b19297fac97011674ef95c62b18cb2dd8e7
Mark A. Grondona <mark.grondona@gmail.com>
2023-01-24 14:05:16 -0800

    libjobspec: add RFC 31 constraint class
    Problem: Fluxion does not support the full RFC 31 Job Constraint
    Specification.
    
    Add a new Flux::Jobspec::Constraint class which handles validation
    and application of a constraint object in jobspec, including the
    `properties` operator and `and`, `or`, and `not` boolean logic
    operators.
    
    The Constraint base class has two derived types to handle the
    supported operations, a PropertyConstraint class which compares
    the set of properties in a resource, and a ConditionalConstraint
    class which contains a vector of other Constraints and applies
    the specified boolean operator when the match() method is called.
    
    Later, other derived classes can be added to handle more constraint
    operators.

---
+++ resource/libjobspec/constraint.hpp
@@ -0,0 +1,41 @@
+/*****************************************************************************\
+ * Copyright 2023 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+/*
+ * Parse RFC 31 Constraint from Jobspec into a Constraint object
+ */
+
+#ifndef JOBSPEC_CONSTRAINT_HPP
+#define JOBSPEC_CONSTRAINT_HPP
+
+#include <iostream>
+#include <string>
+#include <yaml-cpp/yaml.h>
+
+#include "resource/schema/resource_base.hpp"
+#include "parse_error.hpp"
+
+namespace Flux {
+namespace Jobspec {
+
+class Constraint {
+public:
+    Constraint () = default;
+    virtual ~Constraint () = default;
+    virtual bool match (const Flux::resource_model::resource_t &resource) const;
+    virtual YAML::Node as_yaml () const;
+};
+
+std::unique_ptr<Constraint> constraint_parser (const YAML::Node &constraint);
+
+} // namespace Jobspec
+} // namespace Flux
+
+#endif // JOBSPEC_CONSTRAINT_HPP
