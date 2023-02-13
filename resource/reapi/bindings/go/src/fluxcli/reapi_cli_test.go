e5fd6e453b65854539c5e4937d64a5d7a3f1f8ec
Claudia <c.misale@ibm.com>
2023-02-13 16:36:03 -0500

    reapi: add Golang CLI bindings
    Go clients need to call REAPI CLI functions.
    
    Add Go bindings for REAPI CLI for resource module
    initialization, destruction, match allocate,
    cancel, and other common functions.

---
+++ resource/reapi/bindings/go/src/fluxcli/reapi_cli_test.go
@@ -0,0 +1,21 @@
+/*****************************************************************************\
+ * Copyright 2023 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+package fluxcli
+
+import "testing"
+
+func TestFluxcli(t *testing.T) {
+	ctx := NewReapiCli()
+	if ctx == nil {
+		t.Errorf("Context is null")
+	}
+
+}
