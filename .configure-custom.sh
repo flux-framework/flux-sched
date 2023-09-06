8b503eb5d809182a48d9f100cfd5846b800c14c7
Tom Scogland <scogland1@llnl.gov>
2023-09-06 16:39:57 -0700

    add cmake build system alongside autotools
    This is a big one, that was originally developed as a series of commits,
    major decisions are summarized below, but the short version is we have a
    cmake-based build system that can be configured and built as though it
    still had autotools.  To use the autotools build system, manually run
    autoreconf to replace the configure script, and then build as usual.
    
    We're adding a fake configure script here so that it has approximately
    the same ergonomics as the autotools build system.  The only thing that
    causes some breakage is that the autogen.sh script is a no-op, so
    autoreconf needs to be run manually to do the autotools side of the
    build system.
    
    To mimic the autotools build system:
    1. respect PYTHON env var
    
      This is a QOL improvement to allow specifying the python version the
      autotools way rather than with the cmake defines.
    
    2. find flux-core by finding flux, match prefix if none specified
    
      @grondo pointed me to a feature I didn't know we had, where if
      flux-core is in path it's automatically found.  This does that in the
      cmake build system both for the way autotools does it, by checking the
      install prefix, and by checking path as well.
    
    Note on yaml-cpp, while they provide an upstream cmake config that I
    would prefer to use, not all of our docker containers actually offer
    that support, so we're still using pkg-config for now.

---
+++ .configure-custom.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ENABLE_VARS="code-coverage|yes|ENABLE_COVERAGE"
+ENABLE_ENABLE_COVERAGE_DOC="enable coverage analysis"
---
+++ cmake/config_testing.cmake
@@ -0,0 +1,46 @@
+if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
+  include(CTest)
+endif()
+
+set(TEST_ENV)
+set(FLUX_CO_INST "")
+if(FLUX_PREFIX EQUAL CMAKE_INSTALL_PREFIX)
+  set(FLUX_CO_INST "co")
+endif()
+list(APPEND TEST_ENV "FLUX_SCHED_CO_INST=${FLUX_CO_INST}")
+list(APPEND TEST_ENV "PATH=${FLUX_PREFIX}/bin:$ENV{PATH}")
+list(APPEND TEST_ENV "SHARNESS_TEST_DIRECTORY=${CMAKE_BINARY_DIR}/t")
+list(APPEND TEST_ENV "SHARNESS_TEST_SRCDIR=${CMAKE_SOURCE_DIR}/t")
+list(APPEND TEST_ENV "SHARNESS_BUILD_DIRECTORY=${CMAKE_BINARY_DIR}")
+list(APPEND TEST_ENV "FLUX_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}")
+list(APPEND TEST_ENV "FLUX_SCHED_MOD_DIR=${FLUX_MOD_DIR}")
+list(APPEND TEST_ENV "FLUX_SCHED_EXEC_DIR=${FLUX_CMD_DIR}")
+list(APPEND TEST_ENV "FLUX_SCHED_PYTHON_SITELIB=${PYTHON_INSTALL_SITELIB}")
+
+# add maybe-installtest to the start
+function(flux_add_test)
+  # This odd construction is as close as we can get to
+  # generic argument forwarding
+  set(options "")
+  set(oneValueArgs NAME COMMAND)
+  set(multiValueArgs "")
+
+  cmake_parse_arguments(PARSE_ARGV 0 ARG
+    "${options}" "${oneValueArgs}" "${multiValueArgs}")
+
+  set(__argsQuoted)
+  foreach(__item IN LISTS ARG_UNPARSED_ARGUMENTS)
+    string(APPEND __argsQuoted " [==[${__item}]==]")
+  endforeach()
+  if(TARGET ${ARG_COMMAND})
+      # if it's a target, pull the path from the target
+      set(ARG_COMMAND "$<TARGET_FILE:${ARG_COMMAND}>")
+  endif()
+  cmake_language(EVAL CODE "
+  add_test(
+      NAME ${ARG_NAME}
+      COMMAND ${CMAKE_SOURCE_DIR}/t/scripts/maybe-installtest ${ARG_COMMAND}
+      ${__argsQuoted}
+    ) ")
+    set_property(TEST ${ARG_NAME} PROPERTY ENVIRONMENT "${TEST_ENV}")
+endfunction()
