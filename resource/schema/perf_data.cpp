5ea68378ba1ced13904355aba5655904bfbb843b
Daniel Milroy <daniel.milroy@gmail.com>
2024-05-31 00:05:58 -0700

    schema: add global match stats struct
    Problem: data members of the current struct containing performance stats
    need to be modified by various parts of Fluxion.
    
    To avoid replumbing traverser and other functions and changing APIs,
    make `perf` a global variable. Add a header and implementation so other
    components of Fluxion can modify `perf`.

---
+++ resource/schema/perf_data.cpp
@@ -0,0 +1,29 @@
+/*****************************************************************************\
+ * Copyright 2024 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+extern "C" {
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+}
+
+#include "resource/schema/perf_data.hpp"
+
+namespace Flux {
+namespace resource_model {
+
+struct match_perf_t perf;
+
+} // Flux::resource_model
+} // Flux
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
