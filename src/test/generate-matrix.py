ce1a4a2d15f2cf3903d541aa451f57382e2d7227
Tom Scogland <scogland1@llnl.gov>
2024-07-29 12:33:09 -0700

    actions/docker: add el9,f40,noble drop focal
    problem: we're using old images and out of date WRT core
    
    solution: pull in current versions and update build matrix

--- src/test/generate-matrix.py
+++ src/test/generate-matrix.py
@@ -47,12 +47,13 @@ class BuildMatrix:
     def add_build(
         self,
         name=None,
-        image="bookworm",
+        image="fedora40",
         args=default_args,
         jobs=4,
         env=None,
         docker_tag=False,
         coverage=False,
+        coverage_flags=None,
         recheck=True,
         platform=None,
         command_args="",
@@ -61,6 +62,14 @@ class BuildMatrix:
 
         # Extra environment to add to this command:
         env = env or {}
+
+        # hwloc tries to look for opengl devices  by connecting to a port that might
+        # sometimes be an x11 port, but more often for us is munge, turn it off
+        env["HWLOC_COMPONENTS"] = "-gl"
+        # the psm3 connector added to libfabrics in ~1.12 causes errors when allowed to
+        # use non-local connectors on a system with virtual NICs, since we're in a
+        # docker container, prevent this
+        env["PSM3_HAL"] = "loopback"
 
         needs_buildx = False
         if platform:
@@ -101,6 +110,7 @@ class BuildMatrix:
                 "tag": self.tag,
                 "branch": self.branch,
                 "coverage": coverage,
+                "coverage_flags": coverage_flags,
                 "docker_tag": docker_tag,
                 "needs_buildx": needs_buildx,
                 "create_release": create_release,
@@ -116,9 +126,6 @@ class BuildMatrix:
 
 matrix = BuildMatrix()
 
-# Debian: no args
-matrix.add_build(name="bookworm")
-
 # # Debian: 32b -- NOTE: VERY broken right now
 # matrix.add_build(
 #     name="bookworm - 32 bit",
@@ -127,19 +134,23 @@ matrix.add_build(name="bookworm")
 #     docker_tag=True,
 # )
 
-# Debian: arm64, expensive, only on master and tags, only install
+# debian/Fedora40: arm64, expensive, only on master and tags, only install
 if matrix.branch == "master" or matrix.tag:
-    matrix.add_build(
-        name="bookworm - arm64",
-        image="bookworm",
-        platform="linux/arm64",
-        docker_tag=True,
-        command_args="--install-only ",
-    )
+    for d in ("bookworm", "noble", "fedora40", "el9"):
+        matrix.add_build(
+            name=f"{d} - arm64",
+            image=f"{d}",
+            platform="linux/arm64",
+            docker_tag=True,
+            command_args="--install-only ",
+        )
+
+# builds to match arm64 images must have linux/amd64 platform explicitly
 
 # Debian: gcc-12, distcheck
 matrix.add_build(
     name="bookworm - gcc-12,distcheck",
+    image="bookworm",
     env=dict(
         CC="gcc-12",
         CXX="g++-12",
@@ -148,59 +159,74 @@ matrix.add_build(
     args="",
 )
 
-# Debian: coverage
-matrix.add_build(name="coverage", coverage=True, jobs=2)
-
-# Debian: py3.7,clang-6.0
+# fedora40: clang-18
 matrix.add_build(
-    name="bookworm - clang-15",
+    name="fedora40 - clang-18",
     env=dict(
-        CC="clang-15",
-        CXX="clang++-15",
+        CC="clang-18",
+        CXX="clang++-18",
         CFLAGS="-O2 -gdwarf-4",
         chain_lint="t",
-        TEST_CHECK_PREREQS="t",
     ),
-    args='CXXFLAGS="-gdwarf-4"',
+)
+
+# coverage
+matrix.add_build(
+    name="coverage",
+    image="bookworm",
+    coverage_flags="ci-basic",
+    coverage=True,
+    jobs=4,
+)
+
+
+# Ubuntu: TEST_INSTALL
+matrix.add_build(
+    name="noble - test-install",
+    image="noble",
+    env=dict(
+        TEST_INSTALL="t",
+    ),
+    platform="linux/amd64",
+    docker_tag=True,
+)
+
+# el9: TEST_INSTALL
+matrix.add_build(
+    name="el9 - test-install",
+    image="el9",
+    env=dict(
+        TEST_INSTALL="t",
+    ),
+    docker_tag=True,
 )
 
 # Debian: TEST_INSTALL
 matrix.add_build(
     name="bookworm - test-install",
-    env=dict(TEST_INSTALL="t"),
+    image="bookworm",
+    env=dict(
+        TEST_INSTALL="t",
+    ),
     platform="linux/amd64",
+    args="",
     docker_tag=True,
 )
 
-# Ubuntu: TEST_INSTALL
+# Ubuntu 24.04
 matrix.add_build(
-    name="jammy - test-install",
-    image="jammy",
-    env=dict(TEST_INSTALL="t"),
-    docker_tag=True,
-)
-
-# Ubuntu 20.04: py3.8
-matrix.add_build(
-    name="focal",
-    image="focal",
-    docker_tag=True,
-)
-
-# Ubuntu 20.04: py3.8
-matrix.add_build(
-    name="focal - golang-test",
-    image="focal-golang",
+    name="noble - golang-test",
+    image="noble-golang",
     env=dict(
         WITH_GO="yes",
     ),
+    args="",
 )
 
 # RHEL8 clone
 matrix.add_build(
     name="el8",
     image="el8",
-    docker_tag=True,
     env=dict(
         # this is _required_ because of valgrind's new dependency on python3.11
         # which confuses rhel8 cmake's detection logic
@@ -208,18 +234,14 @@ matrix.add_build(
     ),
 )
 
-# Fedora34
+# Fedora 40
 matrix.add_build(
-    name="fedora34",
-    image="fedora34",
+    name="fedora40 - gcc-14",
+    image="fedora40",
+    args=("--prefix=/usr" " --sysconfdir=/etc" " --localstatedir=/var"),
+    platform="linux/amd64",
     docker_tag=True,
 )
 
-# Fedora38
-matrix.add_build(
-    name="fedora38",
-    image="fedora38",
-    docker_tag=True,
-)
 
 print(matrix)
