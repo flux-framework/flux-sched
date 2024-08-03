88f5b139baf02a8ab507fd5eb5e4af0de371baa1
Tom Scogland <scogland1@llnl.gov>
2024-08-02 19:34:38 -0700

    checks_run: remove output_sync for ctest
    problem: output_sync jams all the output from ctest into a single batch,
    so unlike with regular check targets it actually makes things worse
    rather than better
    
    solution: remove the output_sync flag for sched

--- src/test/checks_run.sh
+++ src/test/checks_run.sh
@@ -28,7 +28,7 @@
 # if make is old, and scl is here, and devtoolset is available and not turned
 # on, re-exec ourself with it active to get a newer make
 if make --version | grep 'GNU Make 4' 2>&1 > /dev/null ; then
-  MAKE="make --output-sync=line --no-print-directory"
+  MAKE="make --no-print-directory"
 else
   MAKE="make" #use this if all else fails
   if test "X$X_SCLS" = "X" ; then
