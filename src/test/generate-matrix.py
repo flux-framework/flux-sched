df224f27c1d9a5af1c1906ddb3616fd604c1bb39
Tom Scogland <scogland1@llnl.gov>
2024-08-19 12:05:00 -0700

    matrix: add alpine back in
    problem: we're not testing on alpine in ci
    
    solution: add it back to the matrix

--- src/test/generate-matrix.py
+++ src/test/generate-matrix.py
@@ -198,15 +198,14 @@ matrix.add_multiarch_build(
         CHECK_RUN_SOURCE_ENV="/opt/rh/gcc-toolset-13/enable",
     ),
 )
-# TODO: we have test failures here, EPROTO and notify problems
-# matrix.add_multiarch_build(
-#     name="alpine",
-#     default_suffix=" - test-install",
-#     args=common_args,
-#     env=dict(
-#         TEST_INSTALL="t",
-#     ),
-# )
+matrix.add_multiarch_build(
+    name="alpine",
+    default_suffix=" - test-install",
+    args=common_args,
+    env=dict(
+        TEST_INSTALL="t",
+    ),
+)
 # single arch builds that still produce a container
 matrix.add_build(
     name="fedora40 - test-install",
