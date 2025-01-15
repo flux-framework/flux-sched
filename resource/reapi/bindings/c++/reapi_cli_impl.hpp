9e34b6324474d36fc54ac86cecdbbb7e70e2918b
Daniel Milroy <daniel.milroy@gmail.com>
2025-01-14 18:23:56 -0800

    reapi: add C++ API remove_subgraph functionality
    Problem: there is no C++ API function or functionality to remove a
    subgraph from an existing resource graph.
    
    Add the C++ API function and implementation to remove a subgraph rooted
    at the given path to a vertex and reinitialize the traverser to update
    pruning filter counts.

--- resource/reapi/bindings/c++/reapi_cli_impl.hpp
+++ resource/reapi/bindings/c++/reapi_cli_impl.hpp
@@ -378,6 +378,21 @@ int reapi_cli_t::add_subgraph (void *h, const std::string &R_subgraph)
     return rc;
 }
 
+int reapi_cli_t::remove_subgraph (void *h, const std::string &subgraph_path)
+{
+    resource_query_t *rq = static_cast<resource_query_t *> (h);
+    int rc = -1;
+
+    rq->clear_resource_query_err_msg ();
+    if ((rc = rq->remove_subgraph (subgraph_path)) != 0) {
+        m_err_msg += __FUNCTION__;
+        m_err_msg += ": ERROR: remove_subgraph error: " + std::string (strerror (errno)) + "\n";
+        m_err_msg += rq->get_resource_query_err_msg () + "\n";
+    }
+
+    return rc;
+}
+
 int reapi_cli_t::match_allocate_multi (void *h,
                                        bool orelse_reserve,
                                        const char *jobs,
@@ -1292,6 +1307,24 @@ int resource_query_t::add_subgraph (const std::string &R_subgraph)
 
 out:
     // Close the interner storage after adding the subgraph
+    return rc;
+}
+
+int resource_query_t::remove_subgraph (const std::string &subgraph_path)
+{
+    int rc = -1;
+
+    if ((rc = traverser->remove_subgraph (subgraph_path)) != 0) {
+        m_err_msg = __FUNCTION__;
+        m_err_msg += ": ERROR: traverser returned error: ";
+        m_err_msg += traverser->err_message () + "\n";
+        return rc;
+    }
+    if ((rc = traverser->initialize ()) != 0) {
+        m_err_msg = __FUNCTION__;
+        m_err_msg += ": ERROR: reinitialize traverser after subgraph removal. ";
+        m_err_msg += traverser->err_message () + "\n";
+    }
     return rc;
 }
 
