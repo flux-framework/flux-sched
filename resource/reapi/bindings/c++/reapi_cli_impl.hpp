72354427c067ff316828a17cb5787d9e16241ad7
Daniel Milroy <daniel.milroy@gmail.com>
2025-01-14 18:23:56 -0800

    reapi: add shrink functionality to C++ API

--- resource/reapi/bindings/c++/reapi_cli_impl.hpp
+++ resource/reapi/bindings/c++/reapi_cli_impl.hpp
@@ -181,6 +181,21 @@ int reapi_cli_t::add_subgraph (void *h, const std::string &R_subgraph)
     return rc;
 }
 
+int reapi_cli_t::remove_subgraph (void *h, const std::string &subgraph_path)
+{
+    resource_query_t *rq = static_cast<resource_query_t *> (h);
+    int rc = -1;
+
+    rq->clear_resource_query_err_msg ();
+    if ((rc = rq->remove_subgraph (subgraph_path)) != 0) {
+        m_err_msg += __FUNCTION__;
+        m_err_msg += ": ERROR: remove_subgraph error: " + std::string (strerror (errno)) + "\n";
+        m_err_msg += rq->get_resource_query_err_msg () + "\n";
+    }
+
+    return rc;
+}
+
 int reapi_cli_t::match_allocate_multi (void *h,
                                        bool orelse_reserve,
                                        const char *jobs,
@@ -787,6 +802,39 @@ int resource_query_t::add_subgraph (const std::string &R_subgraph)
     if ((rc = traverser->initialize (db, matcher)) != 0) {
         m_err_msg = __FUNCTION__;
         m_err_msg += ": ERROR: reinitializing traverser after adding subgraph. ";
+        m_err_msg += reader->err_message () + "\n";
+    }
+    return rc;
+}
+
+int resource_query_t::remove_subgraph (const std::string &subgraph_path)
+{
+    int rc = -1;
+    std::shared_ptr<resource_reader_base_t> reader;
+
+    if (subgraph_path == "") {
+        m_err_msg = __FUNCTION__;
+        m_err_msg += ": ERROR: subgraph string is empty\n";
+        errno = EINVAL;
+        return rc;
+    }
+    // No need to check if params.load_format != "jgf" since
+    // remove_subgraph takes a subgraph path, which is
+    // supported by any resource graph
+    if ((reader = create_resource_reader ("jgf")) == nullptr) {
+        m_err_msg = __FUNCTION__;
+        m_err_msg += ": ERROR: can't create JGF reader\n";
+        return rc;
+    }
+    if ((rc = traverser->remove_subgraph (subgraph_path)) != 0) {
+        m_err_msg = __FUNCTION__;
+        m_err_msg += ": ERROR: reader returned error: ";
+        m_err_msg += reader->err_message () + "\n";
+        return rc;
+    }
+    if ((rc = traverser->initialize (db, matcher)) != 0) {
+        m_err_msg = __FUNCTION__;
+        m_err_msg += ": ERROR: reinitialize traverser after shrink. ";
         m_err_msg += reader->err_message () + "\n";
     }
     return rc;
