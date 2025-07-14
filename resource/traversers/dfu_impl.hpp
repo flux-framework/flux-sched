d3774095fa56d254c5a2395df4c052d5a5ee3bbd
jacobtkeio <jacobtkeio@gmail.com>
2025-07-14 16:51:48 -0700

    dfu_impl: Check for negative jobspec duration
    Problem: Checks for negative jobspec duration typically occur before
    submission to the traverser, but some code paths do not perform this
    check.
    
    Check for negative jobspec durations in dfu_impl. This is cheap and
    centralizes the check.

--- resource/traversers/dfu_impl.hpp
+++ resource/traversers/dfu_impl.hpp
@@ -84,6 +84,10 @@ struct jobmeta_t {
             errno = EINVAL;
             return -1;
         }
+        if (jobspec.attributes.system.duration < 0.0f) {
+            errno = EINVAL;
+            return -1;
+        }
         if (jobspec.attributes.system.duration == 0.0f) {
             duration = g_duration;
         } else {
