9bd5fe3f5cfe69ff641e8e356e1c6f0e595f0f53
Daniel Milroy <daniel.milroy@gmail.com>
2025-07-16 23:45:20 -0700

    reapi: add basic allocation unit tests
    Problem: REAPI does not have any allocation unit testing.
    
    Add unit tests of initialization and allocation via Catch2.

---
+++ resource/reapi/test/reapi_cli_test_c.cpp
@@ -0,0 +1,73 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch_test_macros.hpp>
+#include <resource/reapi/bindings/c/reapi_cli.h>
+#include <fstream>
+#include <iostream>
+
+namespace Flux::resource_model::detail {
+
+TEST_CASE ("Initialize REAPI CLI", "[initialize C]")
+{
+    const std::string options = "{}";
+    std::stringstream buffer;
+    std::ifstream inputFile ("../../../t/data/resource/grugs/tiny.graphml");
+
+    if (!inputFile.is_open ()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    buffer << inputFile.rdbuf ();
+    std::string rgraph = buffer.str ();
+
+    reapi_cli_ctx_t *ctx = nullptr;
+    ctx = reapi_cli_new ();
+    REQUIRE (ctx);
+}
+
+TEST_CASE ("Match basic jobspec", "[match C]")
+{
+    int rc = -1;
+    const std::string options = "{}";
+    std::stringstream gbuffer, jbuffer;
+    std::ifstream graphfile ("../../../t/data/resource/grugs/tiny.graphml");
+    std::ifstream jobspecfile ("../../../t/data/resource/jobspecs/basics/test006.yaml");
+
+    if (!graphfile.is_open ()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    jbuffer << jobspecfile.rdbuf ();
+    std::string jobspec = jbuffer.str ();
+
+    if (!jobspecfile.is_open ()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    gbuffer << graphfile.rdbuf ();
+    std::string rgraph = gbuffer.str ();
+
+    reapi_cli_ctx_t *ctx = nullptr;
+    ctx = reapi_cli_new ();
+    REQUIRE (ctx);
+
+    rc = reapi_cli_initialize (ctx, rgraph.c_str (), options.c_str ());
+    REQUIRE (rc == 0);
+
+    match_op_t match_op = match_op_t::MATCH_ALLOCATE;
+    bool reserved = false;
+    char *R;
+    uint64_t jobid = 1;
+    double ov = 0.0;
+    int64_t at = 0;
+
+    rc = reapi_cli_match (ctx, match_op, jobspec.c_str (), &jobid, &reserved, &R, &at, &ov);
+    CHECK (rc == 0);
+    CHECK (reserved == false);
+    CHECK (at == 0);
+}
+
+}
+
+
+
