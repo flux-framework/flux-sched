8896ed13269bb2cca0de7ed915bcd6f2eef9e002
Daniel Milroy <daniel.milroy@gmail.com>
2025-07-20 23:44:01 -0700

    reapi unit test: add shrink
    Problem: shrinking an allocation does not have any unit testing.
    
    Add unit tests of initialization and allocation shrink via Catch2.

--- resource/reapi/test/reapi_cli_test_cxx.cpp
+++ resource/reapi/test/reapi_cli_test_cxx.cpp
@@ -1,4 +1,4 @@
-#define CATCH_CONFIG_MAIN 
+#define CATCH_CONFIG_MAIN
 
 #include <catch2/catch_test_macros.hpp>
 #include <resource/reapi/bindings/c++/reapi_cli.hpp>
@@ -6,7 +6,8 @@
 
 namespace Flux::resource_model::detail {
 
-TEST_CASE( "Initialize REAPI CLI", "[initialize C++]" ) {
+TEST_CASE ("Initialize REAPI CLI", "[initialize C++]")
+{
     const std::string options = "{}";
     std::stringstream buffer;
     std::ifstream inputFile ("../../../t/data/resource/jgfs/tiny.json");
@@ -23,7 +24,8 @@ TEST_CASE( "Initialize REAPI CLI", "[initialize C++]" ) {
     REQUIRE (ctx);
 }
 
-TEST_CASE( "Match basic jobspec", "[match grow C++]" ) {
+TEST_CASE ("Match and basic jobspec", "[match grow C++]")
+{
     int rc = -1;
     const std::string options = "{}";
     std::stringstream gbuffer, jbuffer;
@@ -55,20 +57,89 @@ TEST_CASE( "Match basic jobspec", "[match grow C++]" ) {
     double ov = 0.0;
     int64_t at = 0;
 
-    rc = detail::reapi_cli_t::match_allocate (ctx.get (), match_op, jobspec, jobid, reserved, R, at, ov);
+    rc = detail::reapi_cli_t::match_allocate (ctx.get (),
+                                              match_op,
+                                              jobspec,
+                                              jobid,
+                                              reserved,
+                                              R,
+                                              at,
+                                              ov);
     REQUIRE (rc == 0);
     REQUIRE (reserved == false);
     REQUIRE (at == 0);
 
     R = "";
     match_op = match_op_t::MATCH_GROW_ALLOCATION;
-    rc = detail::reapi_cli_t::match_allocate (ctx.get (), match_op, jobspec, jobid, reserved, R, at, ov);
+    rc = detail::reapi_cli_t::match_allocate (ctx.get (),
+                                              match_op,
+                                              jobspec,
+                                              jobid,
+                                              reserved,
+                                              R,
+                                              at,
+                                              ov);
     REQUIRE (rc == 0);
     REQUIRE (reserved == false);
     REQUIRE (at == 0);
 }
 
+TEST_CASE ("Match shrink basic jobspec", "[match shrink C++]")
+{
+    int rc = -1;
+    const std::string options = "{\"load_format\": \"rv1exec\"}";
+    std::stringstream gbuffer, jbuffer, cbuffer;
+    std::ifstream graphfile ("../../../t/data/resource/rv1exec/tiny_rv1exec.json");
+    std::ifstream jobspecfile ("../../../t/data/resource/jobspecs/cancel/test018.yaml");
+    std::ifstream cancelfile ("../../../t/data/resource/rv1exec/cancel/rank1_cancel.json");
+
+    if (!graphfile.is_open ()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    if (!jobspecfile.is_open ()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    if (!cancelfile.is_open ()) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+
+    gbuffer << graphfile.rdbuf ();
+    std::string rgraph = gbuffer.str ();
+    jbuffer << jobspecfile.rdbuf ();
+    std::string jobspec = jbuffer.str ();
+    cbuffer << cancelfile.rdbuf ();
+    std::string cancel_string = cbuffer.str ();
+
+    std::shared_ptr<resource_query_t> ctx = nullptr;
+    ctx = std::make_shared<resource_query_t> (rgraph, options);
+    REQUIRE (ctx);
+
+    match_op_t match_op = match_op_t::MATCH_ALLOCATE;
+    bool reserved = false;
+    std::string R = "";
+    uint64_t jobid = 1;
+    double ov = 0.0;
+    int64_t at = 0;
+
+    rc = detail::reapi_cli_t::match_allocate (ctx.get (),
+                                              match_op,
+                                              jobspec,
+                                              jobid,
+                                              reserved,
+                                              R,
+                                              at,
+                                              ov);
+    REQUIRE (rc == 0);
+    REQUIRE (reserved == false);
+    REQUIRE (at == 0);
+
+    bool noent_ok = false;
+    bool full_removal = true;
+    rc = detail::reapi_cli_t::cancel (ctx.get (), jobid, cancel_string, noent_ok, full_removal);
+    REQUIRE (rc == 0);
+    REQUIRE (full_removal == false);
 }
 
-
-
+}  // namespace Flux::resource_model::detail
