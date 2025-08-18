7e8fad2feff4ec655976f63f2bf82d463132d819
jacobtkeio <jacobtkeio@gmail.com>
2025-08-18 13:59:56 -0700

    match_op: Add string_to_match_op
    Problem: In many places in resource/, we switch on a string to create an
    appropriate match_op_t. This is prone to error because misspellings are
    more likely to occur across so many duplicated strings, and any changes
    to the match_op_t enums need to be mirrored correctly across many files.
    
    Create a string_to_match_op function in match_op.h to centralize all
    string -> match_op_t mapping.

--- resource/policies/base/match_op.h
+++ resource/policies/base/match_op.h
@@ -25,6 +25,20 @@ static const char *match_op_to_string (match_op_t match_op)
     }
 }
 
+static const match_op_t string_to_match_op (const char *str)
+{
+    if (strcmp (str, "allocate") == 0)
+        return MATCH_ALLOCATE;
+    else if (strcmp (str, "allocate_with_satisfiability") == 0)
+        return MATCH_ALLOCATE_W_SATISFIABILITY;
+    else if (strcmp (str, "allocate_orelse_reserve") == 0)
+        return MATCH_ALLOCATE_ORELSE_RESERVE;
+    else if (strcmp (str, "satisfiability") == 0)
+        return MATCH_SATISFIABILITY;
+    else
+        return MATCH_UNKNOWN;
+}
+
 static bool match_op_valid (match_op_t match_op)
 {
     if ((match_op != MATCH_ALLOCATE) && (match_op != MATCH_ALLOCATE_W_SATISFIABILITY)
