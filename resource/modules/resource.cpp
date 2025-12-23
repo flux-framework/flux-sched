553c3d8b020ec3c92470d421f850460e364ba719
Daniel Milroy <daniel.milroy@gmail.com>
2025-12-22 17:52:00 -0800

    module: add shrink callback

--- resource/modules/resource.cpp
+++ resource/modules/resource.cpp
@@ -78,6 +78,8 @@ static void set_status_request_cb (flux_t *h,
                                    const flux_msg_t *msg,
                                    void *arg);
 
+static void shrink_request_cb (flux_t *h, flux_msg_handler_t *w, const flux_msg_t *msg, void *arg);
+
 static const struct flux_msg_handler_spec htab[] =
     {{FLUX_MSGTYPE_REQUEST, "sched-fluxion-resource.match", match_request_cb, 0},
      {FLUX_MSGTYPE_REQUEST, "sched-fluxion-resource.match_multi", match_multi_request_cb, 0},
@@ -101,6 +103,7 @@ static const struct flux_msg_handler_spec htab[] =
      {FLUX_MSGTYPE_REQUEST, "sched-fluxion-resource.ns-info", ns_info_request_cb, 0},
      {FLUX_MSGTYPE_REQUEST, "sched-fluxion-resource.params", params_request_cb, 0},
      {FLUX_MSGTYPE_REQUEST, "sched-fluxion-resource.set_status", set_status_request_cb, 0},
+     {FLUX_MSGTYPE_REQUEST, "sched-fluxion-resource.shrink", shrink_request_cb, 0},
      FLUX_MSGHANDLER_TABLE_END};
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1021,6 +1024,35 @@ static void get_property_request_cb (flux_t *h,
         }
     }
     if (flux_respond_pack (h, msg, "{s:o}", "values", resp_array) < 0)
+        flux_log_error (h, "%s", __FUNCTION__);
+
+    return;
+
+error:
+    if (flux_respond_error (h, msg, errno, errmsg.c_str ()) < 0)
+        flux_log_error (h, "%s: flux_respond_error", __FUNCTION__);
+}
+
+static void shrink_request_cb (flux_t *h, flux_msg_handler_t *w, const flux_msg_t *msg, void *arg)
+{
+    std::shared_ptr<resource_ctx_t> ctx = getctx ((flux_t *)arg);
+    std::string errmsg = "";
+    char *subgraph_path = NULL;
+
+    if (flux_request_unpack (msg, NULL, "{s:s}", "subgraph_path", &subgraph_path) < 0)
+        goto error;
+
+    if (subgraph_path == "") {
+        errmsg = "empty subgraph path provided to shrink_resources";
+        goto error;
+    }
+
+    if (run_remove_subgraph (ctx, subgraph_path) != 0) {
+        errmsg = "failed to remove subgraph at path " + std::string (subgraph_path);
+        goto error;
+    }
+
+    if (flux_respond_pack (h, msg, "{}") < 0)
         flux_log_error (h, "%s", __FUNCTION__);
 
     return;
