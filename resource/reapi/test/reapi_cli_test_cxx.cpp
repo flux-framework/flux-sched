dbede37074bfb5041bc07e69f91ae47ee947bd06
jacobtkeio <jacobtkeio@gmail.com>
2026-03-11 14:42:12 -0700

    reapi_test_cxx: Test match_op <-> str idempotence
    Problem: No tests exist for the utility functions that convert between
    match_op_ts and strings, and these functions are prone to error due to
    misspellings in strings.
    Similarly, no test exists for `match_op_valid`.
    
    Add a test to reapi_test_cxx.cpp that checks idempotence running each
    match_op_t through both `match_op_to_string` and `match_op_from_string`.
    Also test that `match_op_valid` returns true for valid match_ops and
    false for invalid match_ops.

--- resource/reapi/test/reapi_cli_test_cxx.cpp
+++ resource/reapi/test/reapi_cli_test_cxx.cpp
@@ -2,6 +2,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <resource/reapi/bindings/c++/reapi_cli.hpp>
+#include <resource/policies/base/match_op.h>
 #include <fstream>
 #include <cstdlib>
 
@@ -23,6 +24,56 @@ TEST_CASE ("Initialize REAPI CLI", "[initialize C++]")
     std::shared_ptr<resource_query_t> ctx = nullptr;
     ctx = std::make_shared<resource_query_t> (rgraph, options);
     REQUIRE (ctx);
+}
+
+TEST_CASE ("Convert between strings and match_op_ts", "[match C++]")
+{
+    errno = 0;
+
+    // MATCH_UNKNOWN is invalid and converts to nullptr + ENOENT
+    CHECK (!match_op_valid (MATCH_UNKNOWN));
+    CHECK (errno == 0);
+
+    CHECK (match_op_to_string (MATCH_UNKNOWN) == nullptr);
+    CHECK (errno == ENOENT);
+    errno = 0;
+
+    // invalid strings convert to MATCH_UNKNOWN + ENOENT
+    CHECK (match_op_from_string ("0xDEADBEEF") == MATCH_UNKNOWN);
+    CHECK (errno == ENOENT);
+    errno = 0;
+
+    // out-of-bounds enum values are invalid and convert to nullptr + EINVAL
+    for (match_op_t OOB_match_op :
+         {(match_op_t)(-1), END_MATCH_OP_T, (match_op_t)(END_MATCH_OP_T + 1)}) {
+        CHECK (!match_op_valid (OOB_match_op));
+        CHECK (errno == 0);
+
+        CHECK (match_op_to_string (OOB_match_op) == nullptr);
+        CHECK (errno == EINVAL);
+        errno = 0;
+    }
+
+    // nullptr converts to MATCH_UNKNOWN + EINVAL (since invalid ptr)
+    CHECK (match_op_from_string (nullptr) == MATCH_UNKNOWN);
+    CHECK (errno == EINVAL);
+    errno = 0;
+
+    // valid match_op_t are valid
+    // and match_op_t <-> string conversion is idempotent
+    for (int op = MATCH_UNKNOWN + 1; op != END_MATCH_OP_T; op++) {
+        CHECK (match_op_valid ((match_op_t)op));
+        CHECK (match_op_from_string (match_op_to_string ((match_op_t)op)) == op);
+    }
+    CHECK (errno == 0);
+
+    // each match_op_t has the correct string representation
+    CHECK (match_op_from_string ("allocate") == MATCH_ALLOCATE);
+    CHECK (match_op_from_string ("allocate_orelse_reserve") == MATCH_ALLOCATE_ORELSE_RESERVE);
+    CHECK (match_op_from_string ("allocate_with_satisfiability")
+           == MATCH_ALLOCATE_W_SATISFIABILITY);
+    CHECK (match_op_from_string ("satisfiability") == MATCH_SATISFIABILITY);
+    CHECK (errno == 0);
 }
 
 TEST_CASE ("Match basic jobspec", "[match C++]")
